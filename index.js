b02ad1e965753272f49fe63dada7ba3f16a8b85f
Denis Vasquez <127034921+Denisexper@users.noreply.github.com>
2023-06-15 17:49:50 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,84 @@
+const searchInput = document.getElementById('searchInput');
+const searchButton = document.getElementById('searchButton');
+const characterResult = document.getElementById('characterResult');
+let starWarsLibrary = []; // variable biblioteca
+
+searchButton.addEventListener('click', () => {
+  const searchText = searchInput.value.trim().toLowerCase();
+
+  if (searchText.length > 0) {
+    searchCharacter(searchText);
+  } else {
+    clearCharacterResult();
+  }
+});
+
+function fetchStarWarsLibrary() {
+  const apiUrl = 'https://swapi.dev/api/people/?format=json';
+
+  fetch(apiUrl)
+    .then((response) => response.json())
+    .then((data) => {
+      starWarsLibrary = data.results;
+    })
+    .catch((error) => console.log(error));
+}
+
+function searchCharacter(searchText) {
+  const character = starWarsLibrary.find((character) => character.name.toLowerCase() === searchText);
+
+  if (character) {
+    fetchCharacterDetails(character);
+  } else {
+    displayCharacterNotFound();
+  }
+}
+
+function fetchCharacterDetails(character) {
+  const characterName = character.name;
+
+  const characterImage = new Image();
+  characterImage.src = `https://starwars-visualguide.com/assets/img/characters/${getCharacterId(character)}.jpg`;
+  characterImage.alt = characterName;
+  characterImage.style.width = '200px';
+  characterImage.style.height = '200px';
+
+  characterImage.addEventListener('load', () => {
+    displayCharacter(characterName, characterImage);
+  });
+
+  characterImage.addEventListener('error', () => {
+    displayCharacter(characterName, null);
+  });
+}
+
+function getCharacterId(character) {
+  const urlParts = character.url.split('/');
+  return urlParts[urlParts.length - 2];
+}
+
+function clearCharacterResult() {
+  characterResult.innerHTML = '';
+}
+
+function displayCharacter(characterName, characterImage) {
+  characterResult.innerHTML = '';
+
+  const characterNameElement = document.createElement('h2');
+  characterNameElement.textContent = characterName;
+
+  characterResult.appendChild(characterNameElement);
+
+  if (characterImage) {
+    characterResult.appendChild(characterImage);
+  } else {
+    characterResult.innerHTML += '<p>No se pudo cargar la imagen del personaje.</p>';
+  }
+}
+
+function displayCharacterNotFound() {
+  characterResult.innerHTML = '<p>Personaje no encontrado :(</p>';
+}
+
+fetchStarWarsLibrary();
+
